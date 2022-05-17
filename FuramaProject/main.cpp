5cadbc9707f99778f4ae790579e8fd9a51016613
AnhTuan1407 <anhtuan2003147@gmail.com>
2022-05-17 10:52:25 +0700

    Furama

--- FuramaProject/main.cpp
+++ FuramaProject/main.cpp
@@ -12,34 +12,7 @@
 
 int main() {
 
+    FuramaController furamaController;
+    furamaController.displayMainMenu();
 
-
-//    Person *employee;
-//    employee = new Employee("1", "Anh Tuan", "14/07/03", "male", "1", "0369705323", "anhtuan2003", "Master", "Master",
-//                            1000);
-//    employee->output();
-//
-//    Person* customer;
-//    customer = new Customer("1", "Tuan", "29/07/03", "male", "1", "0369705323", "anhtuan", "VIP", "Da Nang");
-//    customer->output();
-//
-//    Facility* room = new Room("1","room",12,12,5,"asd","hello");
-//    room->output();
-//
-//    Facility* house = new House("2", "taxi", 20, 1000, 4, "normal", "Happy", 3);
-//    house->output();
-//
-//    Facility* villa = new Villa("3", "public", 40, 3000, 10, "VIP", "Luxury", 12, 5);
-//    villa->output();
-
-FuramaController furamaController;
-furamaController.displayMainMenu();
-
-ReadAndWriteEmployee readAndWriteEmployee;
-list<Employee> list = readAndWriteEmployee.readAllEmployee("C:\\Users\\PC\\Desktop\\C++\\Furama\\FuramaProject\\data\\employee");
-for(Employee e : list){
-    e.output();
-}
-readAndWriteEmployee.writeAllEmployee("C:\\Users\\PC\\Desktop\\C++\\Furama\\FuramaProject\\data\\output", list);
-    return 0;
 }
